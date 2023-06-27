59fbd883a988f0246171086eb5f8743ba95df5e4
pavankumar <puttupavankumar@gmail.com>
2023-06-27 16:33:49 +0530

    Changes to be committed: 	new file:   src/30daysReact.txt 	modified:   src/App.js 	new file:   src/about.js 	new file:   src/contact.js 	new file:   src/home.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-function App(probs) {
+import home from "./home"
+import about from "./about"
+import contact from "./contact"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
-  return (
-    <div className='container'>
-      <div>
-        {probs.name}
-      </div>
-      <div>{probs.price}</div>
-      <div>{probs.change}</div>
-      <div>{probs.trade}</div>
-    </div>
-  );
+// function App(probs) {
+
+//   return (
+//     <div className='container'>
+//       <div>
+//         {probs.name}
+//       </div>
+//       <div>{probs.price}</div>
+//       <div>{probs.change}</div>
+//       <div>{probs.trade}</div>
+//     </div>
+//   );
+// }
+function Nav(){
+  <u>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </u>
 }
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element="{<home.js/>}"/>
+    <Route path="/about" element="{<about.js/>}"/>
+    <Route path="/contact" element="{<contact.js/>}"/>
+  </Routes>
+</BrowserRouter>
 
 export default App;
